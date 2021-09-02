b926845c108fa5a50e12be3a3fe99a6868475010
silveryfu <silveryfu@gmail.com>
2021-09-02 00:39:17 -0700

    [blindsig] wip: sender/singer servers

--- pkg/blindsig/server/server.go
+++ pkg/blindsig/server/server.go
@@ -1,20 +1,19 @@
 package server
 
 import (
-	"flag"
+	"context"
 	"sync"
 
 	pb "cellbricks/LoA/pkg/blindsig/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
-var (
-	port = flag.Int("port", 10000, "The server port")
-)
 
 type senderServer struct {
 	pb.SenderServer
 
-	mu sync.Mutex // protects routeNotes
+	mu sync.Mutex
 }
 
 func NewSenderServer() *senderServer {
@@ -22,3 +21,25 @@ func NewSenderServer() *senderServer {
 	return s
 }
 
+func (s *senderServer) Gen(context.Context, *pb.GenRequest) (*pb.GenResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Gen not implemented")
+}
+
+type signerServer struct {
+	pb.SignerServer
+
+	mu sync.Mutex
+}
+
+func NewSignerServer() *signerServer {
+	s := &signerServer{}
+	return s
+}
+
+func (s *signerServer) Sign(context.Context, *pb.SignRequest) (*pb.SignResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Sign not implemented")
+}
+
+func (s *signerServer) Verify(context.Context, *pb.VerifyRequest) (*pb.VerifyResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Verify not implemented")
+}
